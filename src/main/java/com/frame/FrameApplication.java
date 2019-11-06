6bc81f2131f5cb1dbd9f0c3e3745ca3f8b3b4606
jordan <safintel@sina.com>
2019-11-06 10:13:06 +0800

    'fixpage'

--- src/main/java/com/frame/FrameApplication.java
+++ src/main/java/com/frame/FrameApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.frame.web"},repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
+@EnableJpaRepositories(basePackages = {"com.frame.web.business.dao", "com.frame.web.base.attachement"}, repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
 public class FrameApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(FrameApplication.class,args);
+        SpringApplication.run(FrameApplication.class, args);
     }
 }
